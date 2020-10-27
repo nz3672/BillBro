31788be121453b7abc4915b66dd6602c3ba19df4
Sarunya Siripirom <sarunya.si@ku.th>
2020-10-27 22:32:29 +0700

    test Unknowing -Tonqs

---
+++ app/src/main/java/com/example/seproject/ControllerHomePage.java
@@ -0,0 +1,5 @@
+package com.example.seproject;
+
+public class ControllerHomePage {
+
+}
--- app/src/main/java/com/example/seproject/MainActivity.java
+++ app/src/main/java/com/example/seproject/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.seproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText inputUsername;
+    private EditText inputPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputUsername = (EditText)findViewById(R.id.inputUsername);
+        inputPass = (EditText)findViewById(R.id.inputPass);
+
+    }
+
+    public void test(View view){
+        Button testBtn = findViewById(R.id.btnLogin);
+        Toast toast = Toast.makeText(this, inputUsername.getText(),Toast.LENGTH_SHORT);
+            toast.show();
+            System.out.println(inputUsername.getText());
+            System.out.println(inputPass.getText());
     }
 }
